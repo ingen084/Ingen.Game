95c450169016260f2483b586bb4512c2db23457f
ingen084 <ingen188@gmail.com>
2018-04-19 23:38:23 +0900

    俺はマップエディタを作っていた…？(記憶喪失)

--- Tools/MapEditor/Views/Game.cs
+++ Tools/MapEditor/Views/Game.cs
@@ -8,62 +8,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Wpf = System.Windows;
+using WIC = SharpDX.WIC;
+using SharpDX.IO;
 
 namespace MapEditor.Views
 {
 	public class Game : D2dImage
 	{
 		TextFormat format;
-		PathGeometry geo;
 		public Game()
 		{
+			if (IsInDesignMode) return;
+
 			ResourceCache.Add("FrameBrush", t => new SolidColorBrush(t, Color.White));
 
 			format = new TextFormat(DirectWriteFactory, "MS Gothic", 15);
 
-			//geo = new PathGeometry(D2dFactory);
-			//using (var sink = geo.Open())
-			//{
-			//	sink.BeginFigure(new Vector2(0, 10), FigureBegin.Filled);
-			//	sink.AddLine(new Vector2(TileWidth / 2, TileHeight / 2));
-			//	sink.AddLine(new Vector2(0, 10 + TileHeight));
-			//	sink.AddLine(new Vector2(TileWidth / 2, TileHeight / 2));
-			//	sink.EndFigure(FigureEnd.Closed);
-			//	sink.Close();
-			//}
+			TileImage = new Image(ImagingFactory, @"D:\ingen\Desktop\tile7.png");
+			SelectedTileImage = new Image(ImagingFactory, @"D:\ingen\Desktop\tileSelected.png");
 
+			ResourceCache.Add("TileImage", t => Bitmap.FromWicBitmap(t, TileImage.FC, new BitmapProperties(new PixelFormat(SharpDX.DXGI.Format.R8G8B8A8_UNorm, AlphaMode.Premultiplied))));
+			ResourceCache.Add("SelectedTileImage", t => Bitmap.FromWicBitmap(t, SelectedTileImage.FC, new BitmapProperties(new PixelFormat(SharpDX.DXGI.Format.R8G8B8A8_UNorm, AlphaMode.Premultiplied))));
 		}
+
+		Image TileImage;
+		Image SelectedTileImage;
 
 		int[,] SampleMap =
 		{
-			{ 0, 0, 0, 0, 1 },
-			{ 0, 1, 1, 0, 1 },
-			{ 0, 1, 1, 0, 1 },
-			{ 0, 0, 0, 0, 1 },
+			{ 1, 1, 1, 1, },
+			{ 1, 0, 0, 1, },
+			{ 1, 0, 0, 1, },
+			{ 1, 1, 1, 1, },
 		};
 
 		const int XOffset = 400;
 		const int YOffset = 0;
 
-		const int TileWidth = 200;
-		const int TileHeight = 150;
+		const float TileScale = .75f;
+		const float TileWidth = 200 * TileScale;
+		const float TileHeight = 150 * TileScale;
 		const double Slope = .75;
 
 		public override void Render(RenderTarget target)
 		{
-			target.Clear(null);
+			target.Clear(Color.Black);
 
 			target.DrawText("モード: タイル設置", format, new RectangleF(5, 5, float.PositiveInfinity, float.PositiveInfinity), ResourceCache["FrameBrush"]);
 
-			for (int x = 0; x < SampleMap.GetLength(1) + 1; x++)
-				target.DrawLine(
-					new Vector2(ToRawX(x * 0.5 * TileWidth + XOffset), ToRawY(x * 0.5 * TileHeight + YOffset)),
-					new Vector2(ToRawX((x * 0.5 - SampleMap.GetLength(0) * 0.5) * TileWidth + XOffset), ToRawY((x * 0.5 + SampleMap.GetLength(0) * 0.5) * TileHeight + YOffset)), ResourceCache["FrameBrush"]);
-
-			for (int y = 0; y < SampleMap.GetLength(0) + 1; y++)
-				target.DrawLine(
-					new Vector2(ToRawX(-y * 0.5 * TileWidth + XOffset), ToRawY(y * 0.5 * TileHeight + YOffset)),
-					new Vector2(ToRawX((SampleMap.GetLength(1) * 0.5 - y * 0.5) * TileWidth + XOffset), ToRawY((SampleMap.GetLength(1) * 0.5 + y * 0.5) * TileHeight + YOffset)), ResourceCache["FrameBrush"]);
 
 			//上から描画していくループ
 			var height = SampleMap.GetLength(0) + SampleMap.GetLength(1) - 1;
@@ -74,61 +66,57 @@ namespace MapEditor.Views
 				{
 					if (y > SampleMap.GetLength(0) - 1) continue;
 
-					target.DrawText($"{h}:{x},{y}", format, new RectangleF(ToRawX((x * 0.5 - y * 0.5) * TileWidth + XOffset), ToRawY((x * 0.5 + y * 0.5) * TileHeight + YOffset + TileHeight * 0.5), float.PositiveInfinity, float.PositiveInfinity), ResourceCache["FrameBrush"]);
+					var rx = (x * 0.5 - y * 0.5) * TileWidth - TileWidth / 2;
+					var ry = (x * 0.5 + y * 0.5) * TileHeight;
+
+					if (SampleMap[y, x] != 0)
+						target.DrawBitmap(ResourceCache["TileImage"],
+							new RectangleF(
+								ToRawX(rx + XOffset),
+								ToRawY(ry + YOffset),
+								ToRawX(TileWidth),
+								ToRawY(TileHeight + 10 * TileScale)), 1, BitmapInterpolationMode.Linear);
+
+					if (MousePoint is Wpf.Point point)
+					{
+						//中心座標
+						var cx = rx + TileWidth / 2 + XOffset;
+						var cy = ry + TileHeight / 2 + YOffset;
+
+						//当たり判定
+						if (rx + XOffset < point.X && point.X <= rx + XOffset + TileWidth
+							&& ry + YOffset < point.Y + YOffset && point.Y <= ry + YOffset + TileHeight
+
+							&& point.Y <= (Slope * (point.X - (cx - TileWidth / 2)) + cy)
+							&& point.Y >= (Slope * (point.X - (cx + TileWidth / 2)) + cy)
+							&& point.Y < (-Slope * (point.X - (cx + TileWidth / 2)) + cy)
+							&& point.Y > (-Slope * (point.X - (cx - TileWidth / 2)) + cy)
+							)
+							target.DrawBitmap(ResourceCache["SelectedTileImage"],
+													new RectangleF(
+														ToRawX(rx + XOffset),
+														ToRawY(ry + YOffset),
+														ToRawX(TileWidth),
+														ToRawY(TileHeight + 10 * TileScale)), 1, BitmapInterpolationMode.Linear);
+					}
 				}
 			}
 
-			//for (int x = 0; x < SampleMap.GetLength(0); x++)
-			//	for (int y = 0; y < SampleMap.GetLength(1); y++)
-			//	{
-			//		var rx = (x * 0.5 - y * 0.5) * TileWidth;
-			//		var ry = (x * 0.5 + y * 0.5) * TileHeight;
+			//for (int x = 0; x < SampleMap.GetLength(1) + 1; x++)
+			//	target.DrawLine(
+			//		new Vector2(ToRawX(x * 0.5 * TileWidth + XOffset), ToRawY(x * 0.5 * TileHeight + YOffset)),
+			//		new Vector2(ToRawX((x * 0.5 - SampleMap.GetLength(0) * 0.5) * TileWidth + XOffset), ToRawY((x * 0.5 + SampleMap.GetLength(0) * 0.5) * TileHeight + YOffset)), ResourceCache["FrameBrush"], 3);
 
-			//		Bitmap bitmap = ResourceCache["TileImage1"];
-
-			//		if (MousePoint is Wpf.Point point)
-			//		{
-			//			//中心座標
-			//			var cx = rx + TileWidth / 2 + XOffset;
-			//			var cy = ry + TileHeight / 2 + YOffset;
-
-			//			//当たり判定
-			//			if (rx + XOffset <= point.X && point.X <= rx + XOffset + TileWidth
-			//				&& ry + YOffset <= point.Y + YOffset && point.Y <= ry + YOffset + TileHeight
-
-			//				&& point.Y <= (Slope * (point.X - (cx - TileHeight)) + cy)
-			//				&& point.Y >= (Slope * (point.X - (cx + TileHeight)) + cy)
-			//				&& point.Y <= (-Slope * (point.X - (cx + TileHeight)) + cy)
-			//				&& point.Y >= (-Slope * (point.X - (cx - TileHeight)) + cy))
-			//				bitmap = null;
-			//		}
-
-			//		//switch (SampleMap[y, x])
-			//		//{
-			//		//	case 1:
-			//		//		bitmap = ResourceCache["TileImage1"];
-			//		//		break;
-			//		//	case 2:
-			//		//		//bitmap = ResourceCache["TileImage2"];
-			//		//		break;
-			//		//	case 3:
-			//		//		//bitmap = ResourceCache["TileImage3"];
-			//		//		break;
-			//		//	case 4:
-			//		//		//bitmap = ResourceCache["TileImage4"];
-			//		//		break;
-			//		//}
-			//		target.DrawRectangle(new RectangleF(ToRawX(rx + XOffset), ToRawY(ry + YOffset), ToRawX(rx + 128 + XOffset), ToRawY(ry + 64 + YOffset)), ResourceCache["TileBrush"]);
-			//		//if (bitmap != null)
-			//		//	target.DrawBitmap(bitmap, new RawRectangleF(ToRawX(rx + XOffset), ToRawY(ry + YOffset), ToRawX(rx + 128 + XOffset), ToRawY(ry + 64 + YOffset)), 1, BitmapInterpolationMode.Linear);
-
-			//		target.DrawText($"{x},{y}", ResourceCache["MainFont"], new RectangleF(ToRawX(rx + XOffset), ToRawY(ry + 10), ToRawX(rx + XOffset + 100), ToRawY(ry + YOffset + 100)), ResourceCache["ForegroundBrush"]);
-			//	}
-
+			//for (int y = 0; y < SampleMap.GetLength(0) + 1; y++)
+			//	target.DrawLine(
+			//		new Vector2(ToRawX(-y * 0.5 * TileWidth + XOffset), ToRawY(y * 0.5 * TileHeight + YOffset)),
+			//		new Vector2(ToRawX((SampleMap.GetLength(1) * 0.5 - y * 0.5) * TileWidth + XOffset), ToRawY((SampleMap.GetLength(1) * 0.5 + y * 0.5) * TileHeight + YOffset)), ResourceCache["FrameBrush"], 3);
 		}
 
 		public override void Dispose()
 		{
+			TileImage.Dispose();
+			SelectedTileImage.Dispose();
 			format.Dispose();
 		}
 	}
---
+++ Tools/MapEditor/Views/Image.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using WIC = SharpDX.WIC;
+using SharpDX.IO;
+
+namespace MapEditor.Views
+{
+	public class Image : IDisposable
+	{
+		WIC.FormatConverter FormatConverter;
+		public WIC.FormatConverter FC => FormatConverter;
+
+		public Image(WIC.ImagingFactory factory, string path)
+		{
+			using (var decoder = new WIC.PngBitmapDecoder(factory))
+			{
+				using (var inputStream = new WIC.WICStream(factory, path, NativeFileAccess.Read))
+					decoder.Initialize(inputStream, WIC.DecodeOptions.CacheOnLoad);
+
+				FormatConverter = new WIC.FormatConverter(factory);
+				FormatConverter.Initialize(decoder.GetFrame(0), WIC.PixelFormat.Format32bppPRGBA);
+			}
+		}
+
+		public void Dispose()
+		{
+			FormatConverter.Dispose();
+		}
+	}
+}
